d892552a5674e76235c1fc88a125d250880a5cf6
Braian De Roo <braianideroo@gmail.com>
2020-03-11 18:52:45 -0300

    added Demerau-Levenshtein distance as filter

--- src/main/scala/markovNamegen/GenerationOption.scala
+++ src/main/scala/markovNamegen/GenerationOption.scala
@@ -55,8 +55,59 @@ case class Excludes(value: String) extends SingleGenerationOption {
   override def check(element: String): Boolean = !element.contains(value)
 }
 
-case class SimilarTo(value: String) extends SingleGenerationOption {
-  override def check(element: String): Boolean = ???
+case class DamerauLevenshteinDistance(value: String, maxDistance: Int, enableTranspositions: Boolean)
+    extends SingleGenerationOptionM {
+  import zio.Ref
+  import zio.ZIO
+  import zio.ZIO._
+
+  private def damerauLevenshteinMatrix(
+    source: String,
+    target: String,
+    enableTranspositions: Boolean
+  ): ZIO[Any, Nothing, Vector[Int]] = {
+    val w = source.length + 1
+    val h = source.length + 1
+    for {
+      costs <- Ref.make[Vector[Int]](Vector.fill(w * h)(0))
+      _     <- foreach_(0 until w)(x => costs.update(_.updated(x, x)))
+      _     <- foreach_(1 until h)(x => costs.update(_.updated(x * w, x)))
+      cost  <- Ref.make[Int](0)
+      _ <- foreach_(1 until w) { x =>
+            foreach_(1 until h) { y =>
+              if (source.charAt(x - 1) == target.charAt(y - 1)) cost.set(0)
+              else cost.set(1)
+
+              cost.get >>= (c =>
+                costs.update(old =>
+                  old.updated(
+                    x + y * w,
+                    List(
+                      old((x - 1) + (y * w)) + 1,
+                      List(
+                        old(x + ((y - 1) * w)) + 1,
+                        old((x - 1) + ((y - 1) * w)) + c
+                      ).min
+                    ).min
+                  )
+                ) <*> when(
+                  enableTranspositions && x > 1 && y > 1 && source.charAt(x) == target.charAt(y - 1) && source
+                    .charAt(x - 1) == target.charAt(y)
+                )(costs.update(old => old.updated(x + y * w, List(old(x + y * w), old(x - 2 + ((y - 2) * w)) + c).min))).unit
+              )
+            }
+          }
+      res <- costs.get
+    } yield res
+  }
+
+  override def check(element: String): UIO[Boolean] =
+    if (element.length == 0) succeed(value.length <= maxDistance)
+    else if (value.length == 0) succeed(element.length <= maxDistance)
+    else
+      for {
+        distance <- damerauLevenshteinMatrix(element, value, enableTranspositions) >>= (v => succeed(v(v.length - 1)))
+      } yield distance <= maxDistance
 }
 
 case class RegexMatch(value: Regex) extends SingleGenerationOption {
