c81c1d6cf553cf9154cc60ca0c36fc97a3c1d787
Braian De Roo <braianideroo@gmail.com>
2020-04-22 19:44:19 -0300

    Added seed based generation 2

--- src/main/scala/markovNamegen/Model.scala
+++ src/main/scala/markovNamegen/Model.scala
@@ -1,131 +1,98 @@
 package markovNamegen
 
+import braianideroo.random.SeedRandom
+import braianideroo.random.value.{ Probability, RandomVIO }
 import markovNamegen.Smoothing.SmoothingF
-import markovNamegen.util.UniqueVector
 import zio.ZIO._
-import zio.random.Random
-import zio.{ random, Ref, ZIO }
+import zio.{ Ref, ZIO }
 
 private[markovNamegen] class Model private (
-  val alphabet: IndexedSeq[String],
   val order: Int,
-  _observations: Ref[Map[String, Ref[UniqueVector[String]]]],
-  _chains: Ref[Map[String, Ref[Vector[Double]]]],
-  val smoothing: SmoothingF
+  probabilities: Ref[Map[String, RandomVIO[Nothing, Option[String]]]],
+  val smoothing: SmoothingF,
+  letters: IndexedSeq[String]
 ) {
+  type ProbVector = Vector[Probability[String]]
 
-  private def accessMapRef[A, B <: Iterable[A]](map: Ref[Map[String, Ref[B]]]): ZIO[Any, Nothing, Map[String, B]] =
+  def generate(context: String): ZIO[SeedRandom, Nothing, Option[String]] =
     for {
-      a   <- map.get
-      res <- foreach(a)(o => o._2.get >>= (x => succeed(o._1, x)))
-    } yield res.toMap
-
-  def observations: ZIO[Any, Nothing, Map[String, UniqueVector[String]]] =
-    accessMapRef[String, UniqueVector[String]](_observations)
-
-  def chains: ZIO[Any, Nothing, Map[String, Vector[Double]]] =
-    accessMapRef[Double, Vector[Double]](_chains)
-
-  def generate(context: String): ZIO[Random, Nothing, Option[String]] =
-    for {
-      maybeChain <- _chains.get >>= (x => succeed(x.get(context)))
+      maybeChain <- probabilities.get >>= (x => succeed(x.get(context)))
       res <- maybeChain match {
-              case Some(ref) =>
-                for {
-                  chain  <- ref.get
-                  index  <- selectIndex(chain)
-                  result = index.map(x => alphabet(x))
-                } yield result
-              case None => ZIO.none
+              case Some(ref) => ref
+              case None      => ZIO.none
             }
     } yield res
 
   def retrain(data: IndexedSeq[String]): ZIO[Any, Nothing, Unit] =
-    train(data) *> buildChains
+    train(data)
 
   private def train(data: IndexedSeq[String]) =
-    foreach_(data) { element =>
-      val d = ("#" * order) + element + "#"
-      foreach_(0 until (d.length - order))(i => addObservation(d, i))
-    }
-
-  private def addObservation(d: String, i: Int) = {
-    val key = d.substring(i, i + order)
     for {
-      maybeValue <- _observations.get >>= (x => succeed(x.get(key)))
-      value <- maybeValue match {
-                case Some(vector) => succeed(vector)
-                case None =>
-                  Ref.make[UniqueVector[String]](UniqueVector()).tap(x => _observations.update(_ + (key -> x)))
-              }
-      _ <- value.update(_ :+ d.charAt(i + order).toString)
+      observations <- Ref.make[Map[String, Ref[ProbVector]]](Map())
+      _ <- foreach_(data) { element =>
+            val parts = getParts(element, order)
+            foreach_(parts)(part =>
+              addLetters(part._1, observations) *>
+                addPart(part, observations)
+            )
+          }
+      // smooth the probabilities
+      _   <- smoothing.provide(observations)
+      obs <- observations.get
+      x   = obs.map(x => (x._1, x._2.get >>= (y => braianideroo.random.value.RandomValue.fromProbabilityIterable(y))))
+      _   <- probabilities.set(x)
+    } yield ()
+
+  private def addLetters(element: String, observations: Ref[Map[String, Ref[ProbVector]]]) = {
+    val l = letters.map(x => Probability(x, 0))
+    for {
+      refVec <- Ref.make[Vector[Probability[String]]](Vector(l: _*))
+      _      <- observations.update(_ + (element -> refVec))
     } yield ()
   }
 
-  private def buildChains =
+  private def addPart(part: (String, String), obs: Ref[Map[String, Ref[ProbVector]]]) =
     for {
-      _    <- _chains.set(Map())
-      keys <- _observations.get >>= (obs => succeed(obs.keys))
-      _    <- foreach_(keys)(context => foreach_(alphabet)(prediction => buildContext(prediction, context)))
-      _    <- smoothing.provide(_chains)
+      maybeObservationRef <- obs.get.map(_.get(part._1))
+      _ <- maybeObservationRef match {
+            case Some(observationRef) =>
+              for {
+                observation      <- observationRef.get
+                probabilityIndex = observation.indexWhere(_.value == part._2)
+                _ <- probabilityIndex match {
+                      case n: Int if n > -1 =>
+                        val prob    = observation(n)
+                        val newProb = Probability(prob.value, prob.probability + 1)
+                        observationRef.update(_.updated(n, newProb))
+                      case _ =>
+                        observationRef.update(_ :+ Probability(part._2, 1))
+                    }
+              } yield ()
+            case None =>
+              for {
+                observationRef <- Ref
+                                   .make[ProbVector](Vector(Probability(part._2, 1)))
+                _ <- obs.update(_ + (part._1 -> observationRef))
+              } yield ()
+          }
     } yield ()
 
-  private def buildContext(prediction: String, context: String) =
-    for {
-      maybeChain <- _chains.get >>= (x => succeed(x.get(context)))
-      value <- maybeChain match {
-                case Some(ref) => succeed(ref)
-                case None =>
-                  Ref
-                    .make[Vector[Double]](Vector())
-                    .tap(x => _chains.update(_ + (context -> x)))
-              }
-      arr <- _observations.get >>= (x => succeed(x.get(context)))
-      matches <- arr match {
-                  case Some(value) =>
-                    value.get >>= (x => succeed(countMatches(x, prediction)))
-                  case None => succeed(0)
-                }
-      _ <- value.update(_ :+ matches)
-    } yield ()
-
-  private def countMatches[A](arr: Iterable[A], v: A) =
-    arr.foldLeft(0)((a, b) =>
-      b match {
-        case s if s == v => a + 1
-        case _           => a
-      }
-    )
-
-  private def selectIndex(chain: Iterable[Double]) = {
-    val (totals, accumulator) =
-      chain.foldLeft((Vector[Double](), 0d))((old, current) =>
-        (
-          old._1 :+
-            current,
-          old._2 + current
-        )
-      )
-
+  def getParts(string: String, order: Int): Vector[(String, String)] = {
     @scala.annotation.tailrec
-    def inner(rand: Double, totals: Iterable[(Double, Int)]): Option[Int] =
-      if (totals.isEmpty) None
+    def inner(string: String, order: Int, res: Vector[(String, String)]): Vector[(String, String)] =
+      if (string.length <= order) res
       else {
-        if (rand < totals.head._1) Some(totals.head._2)
-        else inner(rand - totals.head._1, totals.tail)
+        val aux = string.substring(0, order)
+        inner(string.tail, order, res :+ (aux, string(order).toString))
       }
-
-    for {
-      rand   <- random.nextDouble >>= (x => succeed(x * accumulator))
-      result = inner(rand, totals.zipWithIndex)
-    } yield result
+    inner(("#" * order) + string + "#", order, Vector())
   }
+
 }
 
 object Model {
-  def make(alphabet: IndexedSeq[String], smoothingF: SmoothingF, order: Int): ZIO[Any, Nothing, Model] =
+  def make(smoothingF: SmoothingF, order: Int, letters: IndexedSeq[String]): ZIO[Any, Nothing, Model] =
     for {
-      observations <- Ref.make[Map[String, Ref[UniqueVector[String]]]](Map())
-      chains       <- Ref.make[Map[String, Ref[Vector[Double]]]](Map())
-    } yield new Model(alphabet, order, observations, chains, smoothingF)
+      probabilities <- Ref.make[Map[String, RandomVIO[Nothing, Option[String]]]](Map())
+    } yield new Model(order, probabilities, smoothingF, letters)
 }
